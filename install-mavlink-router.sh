9c30c62afd9a3a2def16d4ade2573d34a450fb38
Kyle Huang <72529534+brokenfloppydisk@users.noreply.github.com>
2022-05-21 22:36:02 -0700

    Update install-mavlink-router.sh

--- install-mavlink-router.sh
+++ install-mavlink-router.sh
@@ -9,18 +9,17 @@ python3 -m pip install "meson>=0.55"
 echo "Cloning mavlink-router (https://github.com/mavlink-router/mavlink-router)"
 
 git clone https://github.com/mavlink-router/mavlink-router --recurse-submodules
-
 cd mavlink-router
 
 echo "Building mavlink-router..."
 
 python3 -m meson setup build .
-
 ninja -C build
 
 echo "Installing mavlink-router..."
 
 sudo ninja -C build install
+sudo touch /etc/mavlink-router/main.conf
 
 echo "Installation complete. Run mavlink-router or see the git repo 
 (https://github.com/mavlink-router/mavlink-router) for more info."
