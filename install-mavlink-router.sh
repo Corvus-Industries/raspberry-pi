a3e6e5df12e65f7c7f1c89b6839d8133253f3bf0
Kyle Huang <72529534+brokenfloppydisk@users.noreply.github.com>
2022-05-21 22:24:06 -0700

    Add mavlink router install script

---
+++ install-mavlink-router.sh
@@ -0,0 +1,25 @@
+mkdir -p ./Documents/
+
+echo "Installing dependencies..."
+
+sudo apt-get install git ninja-build pkg-config gcc g++ systemd python3
+python3 -m pip install meson>=0.55
+
+echo "Cloning mavlink-router (https://github.com/mavlink-router/mavlink-router)"
+
+git clone https://github.com/mavlink-router/mavlink-router --recurse-submodules
+
+cd mavlink-router
+
+echo "Building mavlink-router..."
+
+meson setup build .
+
+ninja -C build
+
+echo "Installing mavlink-router..."
+
+sudo ninja -C build install
+
+echo "Installation complete. Run mavlink-router or see the git repo 
+(https://github.com/mavlink-router/mavlink-router) for more info."
